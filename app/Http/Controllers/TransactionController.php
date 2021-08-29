b563504a0f4ae6d70081a932f22a6c2e5cb676df
Tony Starks <35855530+slik911@users.noreply.github.com>
2021-08-29 23:16:29 +0100

    Update TransactionController.php

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -47,7 +47,7 @@ class TransactionController extends Controller
 
         $known_signature = hash_hmac('sha256', $request->getContent(), $known_token);
         $data = [$known_signature, $known_token, $signature_parts[1]];
-        return $data;
+        // return $data;
 
         // if (! hash_equals($known_signature, $signature_parts[1])) {
         //     throw new UnauthorizedException('Could not verify request signature ' . $signature_parts[1]);
